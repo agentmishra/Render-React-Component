2ba6227436802773ce6df33a01bc53bce704164a
Amit Kumar Mishra <mishra.amitkr@gmail.com>
2022-02-07 23:53:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,9 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+    <p>I am learning React. My life is getting better.</p>
+    </div>
   )
 }
 
